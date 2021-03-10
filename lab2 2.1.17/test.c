2e47e2132b1725c6335a82415eb7b2ba646e7ee0
krishtels <karina.krishtafovich@mail.ru>
2021-03-11 00:22:04 +0300

    Update test.c

--- lab2 2.1.17/test.c
+++ lab2 2.1.17/test.c
@@ -3,12 +3,12 @@
 #include "main.h"
 
 void test() {
-    assert(fabc(YearLength(2,314) - 0.04) < 0.001);
-    assert(fabc(YearLength(315,628) - 3.15) < 0.001);
-    assert(fabc(Distance(10, 13, 5, 26, 10) - 13) < 0.001);
-    assert(fabc(Distance(14, 112, 12, 28, 3) - 84) < 0.001);
-    assert(fabc(Power(100, 1, 2) - 13.34) < 0.001);
-    assert(fabc(Power(90, 3, 10) - 6.67) < 0.001);
+    assert(fabs(YearLength(2,314) - 0.04) < 0.001);
+    assert(fabs(YearLength(315,628) - 3.15) < 0.001);
+    assert(fabs(Distance(10, 13, 5, 26, 10) - 13) < 0.001);
+    assert(fabs(Distance(14, 112, 12, 28, 3) - 84) < 0.001);
+    assert(fabs(Power(100, 1, 2) - 13.34) < 0.001);
+    assert(fabs(Power(90, 3, 10) - 6.67) < 0.001);
 }
 
 #undef main
